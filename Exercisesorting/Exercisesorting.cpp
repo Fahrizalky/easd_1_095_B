03635df84f716d4dcf4d798322d25be23fecb1c9
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-04-07 09:35:26 +0700

    Add project files.

---
+++ Exercisesorting/Exercisesorting.cpp
@@ -0,0 +1,5 @@
+//1.Algoritma ibutuhkan untuk menyelesaikan suatu masalah karena algoritma dapat mempermudah dalam memecahkan suatu masalah dengan bertahap dengan menguraikan masalah menjadi tugas yang lebih kecil
+//2.Dua struktur data yaitu static (contoh : array) serta dinamic (linked list)
+//3.faktor yang mempengaruhi efeseiensi waktu : Speed of the machine,compiler,operating system,programing language,inpput data
+//4.
+//5.Algoritma quadratic : Bubble sort,Selection sort,Insertion sort. Algoritma Loglinear : Merge sort, Quick sort
